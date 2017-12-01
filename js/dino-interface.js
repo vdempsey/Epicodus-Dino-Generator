3a8f406e53450cade39b2d96f2fc3a8d9b01526e
Jessie Waite and Valia Dempsey <valia@>
2017-11-30 16:21:17 -0800

    Style jumbotron and image output

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,18 +1,10 @@
 import { Copy } from './../js/scripts.js';
 
 let displayData = function(results) {
-
-  console.log(results);
-  // results.forEach(function(element) {
-    // let images = results.data.images.original.url;
-  //  $('.output2').text(`Your search of ${search} returned ${<img src="${results.data[0].images.original.url}">}`);
   for(let i=0; i<25; i++) {
      $('.output2').append("<img src=" + results.data[i].images.original.url + ">");
-   console.log(results.data[i].images.original.url);
  }
-
 };
-
 
 $(document).ready(function() {
 
@@ -22,7 +14,6 @@ $(document).ready(function() {
 
     let newSearch = new Copy(search);
     let apiResults = newSearch.apiConstructor(displayData);
-
   });
 
   $(".btn-error").click(function(event) {
--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,12 @@ export class Copy {
 
   apiConstructor(displayData) {
     let query = this.search;
-    console.log("this is the key in apiConstructor " + apiKey);
     let getDinos = $.get(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=G&lang=en`)
     .then(function(results) {
       displayData(results);
     })
     .fail(function() {
-      console.log('Something went wrong');
+      alert('Sorry, your search returned nothing. Please alter your search and try again.');
     });
 
   }
